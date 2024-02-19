4b15c0520f0725e9f74615d332f0429ef19462b0
Jason Offenbaker <JasonOffenbaker@gmail.com>
2024-02-19 16:06:32 -0700

    one more time on that merge conflict

--- src/components/Certifications/index.js
+++ src/components/Certifications/index.js
@@ -7,10 +7,6 @@ let Certifications = () => {
         <section>
             <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider='' style={{ color: "black" }}>
 
-<<<<<<< HEAD
-                <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
-                </ul>
-=======
                 <div className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
                     {certificationData.certifications.map((cert, index) => (
                         <div key={index}>
@@ -34,7 +30,6 @@ let Certifications = () => {
                     </div>
                     ))}
                 </div>
->>>>>>> certifications
 
                 <a class="uk-position-center-left uk-position-small uk-hidden-hover" href='/' uk-slidenav-previous='' uk-slider-item="previous"></a>
                 <a class="uk-position-center-right uk-position-small uk-hidden-hover" href='/' uk-slidenav-next='' uk-slider-item="next"></a>
