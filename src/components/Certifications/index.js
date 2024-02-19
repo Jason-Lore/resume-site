b3e8cee49d434a8e4bb512572e5a997da51bd5df
Jason Offenbaker <jasonoffenbaker@Jasons-MacBook-Pro.local>
2024-02-19 15:35:45 -0700

    added a json file with my current certs to update the site

--- src/components/Certifications/index.js
+++ src/components/Certifications/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 
-
+import certificationData from './certifications.json'
 
 let Certifications = () => {
     return (
         <section>
-            <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider=''>
+            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider='' style={{ color: "black" }}>
 
-                <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
-                </ul>
+                <div className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+                    {certificationData.certifications.map((cert, index) => (
+                        <div class="uk-card uk-card-hover uk-card-body">
+                            <h3 class="uk-card-title" style={{ color: "black" }}>{cert.certificationName}</h3>
+                            <p>{cert.msftDescription}</p>
+                        </div>
+                    ))}
+                </div>
 
-                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href='/' uk-slidenav-previous='' uk-slider-item="previous"></a>
-                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href='/' uk-slidenav-next='' uk-slider-item="next"></a>
+                <a className="uk-position-center-left uk-position-small uk-hidden-hover" href='/' uk-slidenav-previous='' uk-slider-item="previous"></a>
+                <a className="uk-position-center-right uk-position-small uk-hidden-hover" href='/' uk-slidenav-next='' uk-slider-item="next"></a>
 
             </div>
         </section>
