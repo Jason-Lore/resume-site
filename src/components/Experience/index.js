29b391eaf821287fb77265f316812bb59d100714
Jason Offenbaker <JasonOffenbaker@gmail.com>
2024-02-19 17:34:25 -0700

    added navigatable links to the href

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -5,9 +5,9 @@ let Experience = () => {
         <section>
             <div class="uk-margin-medium-top">
                 <ul class="uk-flex-center" uk-tab='' uk-switcher=''>
-                    <li class="uk-active"><a href="#">Technical</a></li>
-                    <li><a href="#">Management</a></li>
-                    <li><a href="#">Customer Service</a></li>
+                    <li class="uk-active"><a href="/">Technical</a></li>
+                    <li><a href="/">Management</a></li>
+                    <li><a href="/">Customer Service</a></li>
                 </ul>
                 <ul class="uk-switcher uk-margin">
                     <li>am know Tech</li>
