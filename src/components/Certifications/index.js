2eca1365e9b29a7375067a758f5f8e8961ce71e5
Jason Offenbaker <JasonOffenbaker@gmail.com>
2024-02-19 17:14:10 -0700

    centered certifications

--- src/components/Certifications/index.js
+++ src/components/Certifications/index.js
@@ -5,7 +5,7 @@ import certificationData from './certifications.json'
 let Certifications = () => {
     return (
         <section>
-            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider='' style={{ color: "black" }}>
+            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1" uk-slider='center: true' style={{ color: "black" }}>
 
                 <div className="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
                     {certificationData.certifications.map((cert, index) => (
