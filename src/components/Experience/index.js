aee9f93483a772cde1d9d807c8e1d50b1db46c52
Jason Offenbaker <JasonOffenbaker@gmail.com>
2024-03-03 18:29:44 -0700

    rearranged certification titles and added some. additionally added a Who am I tab and updated the technical

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 
 let Experience = () => {
+
+    const d = new Date();
+    let year = d.getFullYear();
+
     return (
         <section>
             <div class="uk-margin-medium-top">
                 <ul class="uk-flex-center" uk-tab='' uk-switcher=''>
-                    <li class="uk-active"><a href="/">Technical</a></li>
+                    <li class="uk-active"><a href="/">Who I am</a></li>
+                    <li><a href="/">Technical</a></li>
                     <li><a href="/">Management</a></li>
                     <li><a href="/">Customer Service</a></li>
                 </ul>
-                <ul class="uk-switcher uk-margin">
-                    <li>am know Tech</li>
+                <ul class="uk-switcher uk-margin glass-pane">
+                    <li>
+                        <div class='white-text'>
+                            <h2>Jason Lore</h2>
+                        </div>
+                    </li>
+                    <li>
+                        <div class='white-text'>
+                            <p>{year - 2020}+ years of Techncial Experience in React, PowerShell, Python, and more!</p>
+                            <p>Escalations Engineer and Technical Advisor for Microsoft</p>
+                            <p>Constantly Learning new Technologies and Studying new Certifications</p>
+                        </div>
+                    </li>
                     <li>I managed for like...2 months before my whole team was laid off</li>
                     <li>Master of blending arts</li>
                 </ul>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,20 +7,10 @@ let Header = () => {
                 <div class="uk-container">
                     <div uk-navbar=''>
                         <div class="uk-navbar-center">
-                            <div class="uk-navbar-center-left">
-                                <ul class="uk-navbar-nav">
-                                    <li><a href="/Certifications">Certifications</a></li>
-                                </ul>
-                            </div>
+                            <a class="uk-navbar-item " href="/">Certifications</a>
                             <a class="uk-navbar-item uk-logo" href="/">Jason Lore</a>
-                            <div class="uk-navbar-center-right">
-                                <ul class="uk-navbar-nav">
-                                    <li><a href="/Resume">Resume</a></li>
-                                </ul>
-                            </div>
-
+                            <a class="uk-navbar-item " href="/">Resume</a>
                         </div>
-
                     </div>
                 </div>
             </nav>
