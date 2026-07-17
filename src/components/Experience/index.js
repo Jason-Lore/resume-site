1fe1f918debade8995e4c3c764a7002d62fd7e59
Jason <jason@Thelorestudio.com>
2026-07-16 19:57:37 -0700

    refreshed Who am I screen

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -9,32 +9,35 @@ let Experience = () => {
         <section>
             <div class="uk-margin-medium-top container">
                 <ul class="uk-flex-center visible-text" uk-tab='' uk-switcher=''>
-                    <li class="uk-active"><a class="visible-text"href="/">Who I am</a></li>
+                    <li class="uk-active"><a class="visible-text" href="/">Who I am</a></li>
                     <li class="uk-active"><a href="/">Technical</a></li>
-                    <li class="uk-active"><a class="visible-text"href="/">Management</a></li>
+                    <li class="uk-active"><a class="visible-text" href="/">Management</a></li>
                 </ul>
                 <ul class="uk-switcher uk-margin glass-pane white-text">
                     <li>
                         <div>
-                            <p>My name is Jason Lore,</p>
-                            <p>I am a Level 3 Microsoft365 Escalations Engineer working with Microsoft as a Partner.</p>
-                            <p>My current studies are targeted towards increasing my knowledge and understanding of modern JavaScript libraries, PowerApps, Azure Services, and Machine Learning Models.</p>
-                            <p>I love learning new things and facing challenges head on with creative out of the box solutions.</p>
-                            <p>I went to a Coding bootcamp hosted by the University of Arizona that went over MongoDB, Express, React, Node, and Computer Science topics.</p>
+                            <p>
+                                My name is Jason Lore, and I'm an engineer who enjoys building the bridge between people and technology.
+                            </p>
+
+                            <p>
+                                My experience spans Microsoft 365, Microsoft Copilot, Azure, automation, software development,
+                                and enterprise infrastructure, with a focus on making complex systems easier to manage and more
+                                efficient to operate.
+                            </p>
+
+                            <p>
+                                Throughout my career I've deployed enterprise cloud solutions, developed automation using
+                                Microsoft Graph API and PowerShell, and built internal tools that improve engineering workflows
+                                and reduce manual effort.
+                            </p>
+
+                            <p>
+                                I'm passionate about AI, cloud technologies, and building software that has a measurable impact.
+                                Whether I'm solving difficult technical problems or creating new automation, I'm always looking
+                                for ways to learn, improve, and build something better.
+                            </p>
                         </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>{year - 2020}+ years of Techncial Experience in React, PowerShell, Python, and more!</p>
-                            <p>Escalations Engineer and Technical Advisor for Microsoft</p>
-                            <p>Constantly Learning new Technologies and Studying new Certifications</p>
-                        </div>
-                    </li>
-                    <li>
-                        <p>{year - 2020}+ years of Management experience</p>
-                        <p>From managing breaks to managing complicated technical backlogs</p>
-                        <p>I've seen and conquered all challenged that have come my way.</p>
-                    
                     </li>
                 </ul>
             </div>
